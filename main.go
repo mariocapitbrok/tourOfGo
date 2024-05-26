ca0401891f3776b90cebeeb811104f2dd3279b02
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-25 20:09:46 -0600

    Named return values

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
 
 /*
-Multiple results
-A function can return any number of results.
 
-The swap function returns two strings.
+Named return values
+Go's return values may be named. If so, they are treated as variables defined at the top of the function.
+
+These names should be used to document the meaning of the return values.
+
+A return statement without arguments returns the named return values. This is known as a "naked" return.
+
+Naked return statements should be used only in short functions, as with the example shown here. They can harm readability in longer functions.
 */
