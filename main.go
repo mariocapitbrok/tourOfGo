1e2ce995c066599b9a14c84095a5c8d5ac8813c5
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-25 20:13:40 -0600

    Variables with initializers

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import "fmt"
 
-var c, python, java bool
+var i, j int = 1, 2
 
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 }
 
-/*
-Variables
-The var statement declares a list of variables; as in function argument lists, the type is last.
 
-A var statement can be at package or function level. We see both in this example.
+/*
+Variables with initializers
+A var declaration can include initializers, one per variable.
+
+If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
 */
