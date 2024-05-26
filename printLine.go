63d93c9c5bf94ff92a1680aa0fb6e71da851d64c
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-25 19:12:20 -0600

    Test printLine

---
+++ printLine.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+
+	fmt.Println("Square toot of 18 is", math.Sqrt(16))
+}
