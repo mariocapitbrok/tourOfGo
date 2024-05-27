2008b999c7c205126a22e8d482026f020b631bba
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-27 07:48:29 -0600

    For is Go's "While"

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	sum := 1
-	for ; sum < 1000; {
+	for sum < 1000 {
 		sum += sum
 	}
 	fmt.Println(sum)
@@ -12,7 +12,8 @@ func main() {
 
 
 
+
 /*
-For continued
-The init and post statements are optional.
+For is Go's "while"
+At that point you can drop the semicolons: C's while is spelled for in Go.
 */
