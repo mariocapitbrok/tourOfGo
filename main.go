f1049636e43e0f4b006a3360b0b5a556b9f16e81
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-27 20:48:55 -0600

    Defer

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon.")
-	default:
-		fmt.Println("Good evening.")
-	}
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
 }
 
-
 /*
-Switch with no condition
-Switch without a condition is the same as switch true.
 
-This construct can be a clean way to write long if-then-else chains.
+Defer
+A defer statement defers the execution of a function until the surrounding function returns.
+
+The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
 */
