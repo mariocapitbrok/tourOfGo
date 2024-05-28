efb591ed2e70a2dc9d426ec0e9f6281e9bb961d2
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-27 20:27:18 -0600

    Refactor function

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 // Sqrt calculates the square root of x using Newton's method
 func Sqrt(x float64) float64 {
 	z:= 1.0 // initial guess
+	prevZ := 0.0
 
-	for i:=0; i < 10; i++ {
+	for math.Abs(z-prevZ) > 1e-10 {
+		prevZ = z
 		z -= (z*z - x) / (2 * z)
-		fmt.Printf("Iteration %d: z = %v\n", i, z)
+		fmt.Printf("z = %v\n", z)
 	}
 
 	return z
