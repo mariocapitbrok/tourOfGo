d43822d14392cf459839119baab196fe449fa3be
Luis Ramos <luis.ramos@salvosoftware.com>
2024-05-27 20:50:29 -0600

    Stacking defers

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("world")
+	fmt.Println("counting")
 
-	fmt.Println("hello")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
 
+
 /*
+Stacking defers
+Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
 
-Defer
-A defer statement defers the execution of a function until the surrounding function returns.
-
-The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+To learn more about defer statements read this blog post.
 */
